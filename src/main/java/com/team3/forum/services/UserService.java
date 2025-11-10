3807441bb775fcd78ea63c1430b6a4eab390774e
teodo <teodoragencheva10685@gmail.com>
2025-11-10 14:39:54 +0200

    Added UserServiceImpl only CRUD methods

--- src/main/java/com/team3/forum/services/UserService.java
+++ src/main/java/com/team3/forum/services/UserService.java
@@ -1,4 +1,14 @@
 package com.team3.forum.services;
 
+import com.team3.forum.models.User;
+
+import java.util.List;
+
 public interface UserService {
+    User save(User entity);
+    User findById(int id);
+    boolean existsById(int id);
+    List<User> findAll();
+    void deleteById(int id);
+    void delete(User entity);
 }
--- src/main/java/com/team3/forum/services/UserServiceImpl.java
+++ src/main/java/com/team3/forum/services/UserServiceImpl.java
@@ -1,4 +1,54 @@
 package com.team3.forum.services;
 
+import com.team3.forum.models.User;
+import com.team3.forum.repositories.UserRepository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User save(User entity) {
+        return userRepository.save(entity);
+    }
+
+
+    @Override
+    @Transactional(readOnly = true)
+    public User findById(int id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public boolean existsById(int id) {
+        return userRepository.existsById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public void deleteById(int id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public void delete(User entity) {
+        userRepository.delete(entity);
+    }
 }
