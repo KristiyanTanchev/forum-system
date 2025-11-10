ab8ea60b1d286022fad412af9feb5e1aa61f86e0
teodo <teodoragencheva10685@gmail.com>
2025-11-10 17:06:25 +0200

    Added UserDtos and UserRestController only CRUD methods

--- src/main/java/com/team3/forum/services/UserServiceImpl.java
+++ src/main/java/com/team3/forum/services/UserServiceImpl.java
@@ -1,6 +1,9 @@
 package com.team3.forum.services;
 
+import com.team3.forum.helpers.UserMapper;
 import com.team3.forum.models.User;
+import com.team3.forum.models.userDtos.UserCreateDto;
+import com.team3.forum.models.userDtos.UserUpdateDto;
 import com.team3.forum.repositories.UserRepository;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,17 +15,27 @@ import java.util.List;
 @Transactional
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
+
+    @Override
+    public User createUser(UserCreateDto dto) {
+        User user = userMapper.toEntity(dto);
+       return userRepository.save(user);
     }
 
     @Override
-    public User save(User entity) {
-        return userRepository.save(entity);
+    public User updateUser(int id, UserUpdateDto dto) {
+       User existingUser = userRepository.findById(id);
+       userMapper.updateEntityFromDto(dto,existingUser);
+       return userRepository.save(existingUser);
     }
-
 
     @Override
     @Transactional(readOnly = true)
