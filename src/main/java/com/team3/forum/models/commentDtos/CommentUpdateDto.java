4b62cd4fee47b96195bc538ffedea79619cd9c19
Ivan <ivan.georgiev03@abv.bg>
2025-11-24 17:24:06 +0200

    auth changes

--- src/main/java/com/team3/forum/models/commentDtos/CommentUpdateDto.java
+++ src/main/java/com/team3/forum/models/commentDtos/CommentUpdateDto.java
@@ -1,6 +1,7 @@
 package com.team3.forum.models.commentDtos;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -10,6 +11,7 @@ import lombok.*;
 @Builder
 public class CommentUpdateDto {
 
-    @NotBlank(message = "Content cannot be blank")
+    @NotBlank(message = "Tag name is required")
+    @Size(min = 2, max = 50, message = "Tag name must be between 2 and 50 characters")
     private String content;
 }
